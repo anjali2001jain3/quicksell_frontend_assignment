2312cb9d6ebde0972def164fe0f32c9805aa44f6
Anjali Jain <anjali2001jain3@gmail.com>
2023-09-08 18:50:58 +0530

    component: create card component

---
+++ src/component/Card.js
@@ -0,0 +1,25 @@
+// Card.js
+import React from "react";
+import "./Card.css";
+
+const Card = ({ title, id, tag }) => (
+  <div className="card">
+    <div className="card-content">
+      <p className="card-id">
+        {id} <br />
+      </p>
+      <h4 className="card-title ">
+        <b>
+          {/* <span className="bullet-icon">•</span> */}
+          {title}
+        </b>
+        <br />
+      </h4>
+      <div className="card-tag">
+        <span>•</span> {tag}
+      </div>
+    </div>
+  </div>
+);
+
+export default Card;
