9b034d682adbecdebe667b92dd89dc448afb134b
Anjali Jain <anjali2001jain3@gmail.com>
2023-09-08 18:57:08 +0530

    App: implement components and style them

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DropDown from "./component/DropDown";
+import "./App.css";
+import Filter from "./component/Filter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tickets: [],
+      dataIsLoaded: false,
+      groupOption: "status",
+      orderOption: "priority",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.quicksell.co/v1/internal/frontend-assignment")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          tickets: json.tickets,
+          dataIsLoaded: true,
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
+
+  render() {
+    const { dataIsLoaded, tickets, groupOption, orderOption } = this.state;
+
+    return (
+      <>
+        {!dataIsLoaded && <div>Loading...</div>}
+        <div className="App">
+          <DropDown
+            className="nav"
+            groupOption={groupOption}
+            setGroupOption={(option) => this.setState({ groupOption: option })}
+            orderOption={orderOption}
+            setOrderOption={(option) => this.setState({ orderOption: option })}
+          />
+          <Filter
+            tickets={tickets}
+            groupOption={groupOption}
+            orderOption={orderOption}
+          />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
